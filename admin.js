7c0f394bc3f88aca1d7912f7e5eccd16f554fcc2
smt-ivr <s6754091@gmail.com>
2026-06-28 17:47:50 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -85,7 +85,8 @@ export async function handleAdminUpdateUser(request, env) {
         }
 
         const finalPassword = newPassword || user.password;
-        const finalEmail = newEmail === undefined ? user.email : (newEmail || null); // הגנה מערכים חסרים
+        // המרה של האימייל החדש לאותיות קטנות (אם הוזן)
+        const finalEmail = newEmail === undefined ? user.email : (newEmail ? String(newEmail).toLowerCase() : null); 
         
         const finalCanUpload = canUpload === undefined ? user.can_upload : (canUpload ? 1 : 0);
         const finalCanRecord = canRecord === undefined ? user.can_record : (canRecord ? 1 : 0);
