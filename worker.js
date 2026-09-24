e2fbd6f28568bd0d6f704cba69d74b36bd396ac8
smt-ivr <s6754091@gmail.com>
2026-09-25 00:59:34 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,19 @@
 // worker.js
+import htmlContent from './index.html';
+import cssContent from './style.css';
+import mainJs from './client-main.js';
+import authJs from './client-auth.js';
+import messagesJs from './client-messages.js';
+import audioJs from './client-audio.js';
+import settingsJs from './client-settings.js';
+import adminJs from './client-admin.js';
+import adsJs from './client-ads.js';
+import adminAdsJs from './client-admin-ads.js';
+import adminAdvancedJs from './client-admin-advanced.js';
+import adminChatJs from './client-admin-chat.js';
+import userChatJs from './client-user-chat.js';
+import smartAdminJs from './client-smart-admin.js';
+
 import { handleCheckIdentifier, handleRegister, handleLogin, handleGoogleLogin, handleGetProfile, handleUpdateProfile, handleChangePassword, handleResetPasswordConfirm, handleLogout, authenticateUser, handleCheckUnsubscribeToken, handleConfirmUnsubscribe, handleUnblockEmail } from './auth.js';
 import { handleAdminLogin, handleAdminGetUsers, handleAdminUpdateUser, handleAdminGetUserFullProfile, handleAdminDisconnectUserTokens, handleAdminCreateUser, handleAdminDeleteUser, handleAdminGetTables, handleAdminExecuteQuery, handleAdminUpdateYemotName, handleAdminGetPermissions, handleAdminGetAuditLogs, handleAdminGetYemotNamesList } from './admin.js';
 import { VerificationSystem } from './verification.js';
@@ -22,8 +37,26 @@ export default {
         if (request.method === "OPTIONS") return new Response(null, { headers: corsHeaders });
 
         const url = new URL(request.url);
-        const pathname = url.pathname.replace(/\/$/, "");
+        const path = url.pathname;
+        const pathname = path.replace(/\/$/, "");
         const userIp = request.headers.get('cf-connecting-ip') || '0.0.0.0';
+
+        if (path === '/salamon' || path === '/salamon/' || path === '/salamon/admin' || path === '/salamon/admin/' || path === '/salamon/unsubscribe' || path === '/salamon/unsubscribe/') {
+            return new Response(htmlContent, { headers: { 'Content-Type': 'text/html; charset=utf-8' } });
+        }
+        if (path === '/salamon/style.css') { return new Response(cssContent, { headers: { 'Content-Type': 'text/css; charset=utf-8' } }); }
+        if (path === '/salamon/client-main.js') { return new Response(mainJs, { headers: { 'Content-Type': 'application/javascript; charset=utf-8' } }); }
+        if (path === '/salamon/client-auth.js') { return new Response(authJs, { headers: { 'Content-Type': 'application/javascript; charset=utf-8' } }); }
+        if (path === '/salamon/client-messages.js') { return new Response(messagesJs, { headers: { 'Content-Type': 'application/javascript; charset=utf-8' } }); }
+        if (path === '/salamon/client-audio.js') { return new Response(audioJs, { headers: { 'Content-Type': 'application/javascript; charset=utf-8' } }); }
+        if (path === '/salamon/client-settings.js') { return new Response(settingsJs, { headers: { 'Content-Type': 'application/javascript; charset=utf-8' } }); }
+        if (path === '/salamon/client-admin.js') { return new Response(adminJs, { headers: { 'Content-Type': 'application/javascript; charset=utf-8' } }); }
+        if (path === '/salamon/client-admin-advanced.js') { return new Response(adminAdvancedJs, { headers: { 'Content-Type': 'application/javascript; charset=utf-8' } }); }
+        if (path === '/salamon/client-ads.js') { return new Response(adsJs, { headers: { 'Content-Type': 'application/javascript; charset=utf-8' } }); }
+        if (path === '/salamon/client-admin-ads.js') { return new Response(adminAdsJs, { headers: { 'Content-Type': 'application/javascript; charset=utf-8' } }); }
+        if (path === '/salamon/client-admin-chat.js') { return new Response(adminChatJs, { headers: { 'Content-Type': 'application/javascript; charset=utf-8' } }); }
+        if (path === '/salamon/client-user-chat.js') { return new Response(userChatJs, { headers: { 'Content-Type': 'application/javascript; charset=utf-8' } }); }
+        if (path === '/salamon/client-smart-admin.js') { return new Response(smartAdminJs, { headers: { 'Content-Type': 'application/javascript; charset=utf-8' } }); }
 
         try {
             let response;
